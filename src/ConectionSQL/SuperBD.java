ca1e707c6d0e353b6514c301cb1c43a9df0aa7e1
AlexisReyes98 <giovanny29.garv@gmail.com>
2023-03-09 22:13:16 -0600

    Subiendo Caso de Estudio Metodología: SCRUM Sprint 1

---
+++ src/ConectionSQL/SuperBD.java
@@ -0,0 +1,132 @@
+
+package ConectionSQL;
+
+import Model.Producto;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alexis
+ */
+public class SuperBD {
+    /*
+    *   Sistema que administra productos de un super
+    */
+    
+    // Código para INSERTAR un nuevo producto en la tabla de la BD
+    public void altaProducto(Producto prod) {
+        Connection cnx = DataBaseConexion.getConnection();
+        try {
+            PreparedStatement pst = cnx.prepareStatement("INSERT INTO PRODUCTOS(CLAVE_PROD,NOMBRE,PROVEEDOR)"
+                    + " VALUES (?,?,?)");
+            pst.setInt(1, prod.getClaveProd());
+            pst.setString(2, prod.getNombre());
+            pst.setString(3, prod.getProveedor());
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error al agregar datos en la tabla");
+        } finally {
+            try {
+                cnx.close();
+            } catch (SQLException ex) {
+                System.out.println("Error: " + ex.getMessage());
+            }
+        }
+    }
+    
+    // Código para generar una nuva clave del producto
+    public int generaClaveProd() {
+        int clave = 0;
+        Connection cnx = DataBaseConexion.getConnection();
+        try {
+            // Se realiza la llamada a la funcion de la BD
+            CallableStatement cst = cnx.prepareCall("{? = call CLAVE_MAYOR (?)}");
+            
+            // Se indica que el primer interrogante es de salida
+            cst.registerOutParameter(1, java.sql.Types.INTEGER);
+            
+            // Se pasa un parámetro en el segundo interrogante
+            cst.setInt(2, 0);
+            
+            // Ejecuta la función almacenada
+            cst.executeUpdate();
+            
+            // Se recupera el resultado de la funcion pl/sql
+            clave = cst.getInt(1);
+            clave = clave + 1;
+        } catch (SQLException ex) {
+            clave = 1;
+            System.out.println(ex.getMessage());
+            System.out.println("Error al obtener la clave mayor o no hay regristros");
+        } finally {
+            try {
+                cnx.close();
+            } catch (SQLException ex) {
+                System.out.println("Error: " + ex.getMessage());
+            }
+        }
+        
+        return clave;
+    }
+    
+    // Código para VER la tabla de la BD
+    public ArrayList<Producto> consultarProductos() {
+        ArrayList<Producto> productos = new ArrayList();
+        Connection cnx = DataBaseConexion.getConnection();
+        try {
+            Statement st = cnx.createStatement();
+            ResultSet rs = st.executeQuery("SELECT CLAVE_PROD,NOMBRE,PROVEEDOR"
+                    + " FROM PRODUCTOS ORDER BY 1 ");
+            while (rs.next()) {
+                Producto prod = new Producto();
+                prod.setClaveProd(rs.getInt("CLAVE_PROD"));
+                prod.setNombre(rs.getString("NOMBRE"));
+                prod.setProveedor(rs.getString("PROVEEDOR"));
+                productos.add(prod);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error al obtener los datos de la tabla");
+        } finally {
+            try {
+                cnx.close();
+            } catch (SQLException ex) {
+                System.out.println("Error: " + ex.getMessage());
+            }
+        }
+        
+        return productos;
+    }
+    
+    // Código para utilizar el Procedimiento almacenado de Oracle PL/SQL (BAJA_PRODUCTO)
+    public void bajaProducto(int clave_producto) {
+        Connection cnx = DataBaseConexion.getConnection();
+        try {
+            // Se realiza la llamada al pocedimiento de la BD
+            CallableStatement cst = cnx.prepareCall("{call BAJA_PRODUCTO (?)}");
+            
+            // Parametro para el procedimiento almacenado
+            cst.setInt(1, clave_producto);
+            
+            // Ejecuta el procedimiento almacenado
+            cst.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error al eliminar el registro de la tabla");
+        } finally {
+            try {
+                cnx.close();
+            } catch (SQLException ex) {
+                System.out.println("Error: " + ex.getMessage());
+            }
+        }
+    }
+    
+}
---
+++ src/Model/Producto.java
@@ -0,0 +1,49 @@
+    
+package Model;
+
+/**
+ *
+ * @author alexis
+ */
+public class Producto {
+    private int claveProd;
+    private String nombre;
+    private String proveedor;
+    
+    // Constructor basio
+    public Producto() {
+    }
+
+    // Constructor
+    public Producto(int claveProd, String nombre, String proveedor) {
+        this.claveProd = claveProd;
+        this.nombre = nombre;
+        this.proveedor = proveedor;
+    }
+
+    // Getters y Setters
+    public int getClaveProd() {
+        return claveProd;
+    }
+
+    public void setClaveProd(int claveProd) {
+        this.claveProd = claveProd;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+    
+}
